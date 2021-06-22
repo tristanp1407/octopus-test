ba8062ccf2cebb1a292abd59fcfc9c24cb5338d2
tristanp1407 <tristan.plet@gmail.com>
2021-06-22 09:23:07 +0100

    updated the Readme

---
+++ octopus/client/build/precache-manifest.47e0f0c9f18377bae7e16ec1dd030baa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f01effb8f008160a1c68721f7eacb1ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d26b809aa013f6eeba0",
+    "url": "/static/css/main.0156ff15.chunk.css"
+  },
+  {
+    "revision": "0a0fcbbd65ce436f5af4",
+    "url": "/static/js/2.259bb6f2.chunk.js"
+  },
+  {
+    "revision": "81fbb27446145c382bc1dab229a0d054",
+    "url": "/static/js/2.259bb6f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d26b809aa013f6eeba0",
+    "url": "/static/js/main.f13d62a2.chunk.js"
+  },
+  {
+    "revision": "df8dabf54dfa42932d06",
+    "url": "/static/js/runtime-main.fc7e3514.js"
+  },
+  {
+    "revision": "563b5bb0ff86a84630dd1481928ca6af",
+    "url": "/static/media/basket.563b5bb0.svg"
+  },
+  {
+    "revision": "ee26f1405b89edcf6e11dd524958c542",
+    "url": "/static/media/logo.ee26f140.svg"
+  }
+]);
